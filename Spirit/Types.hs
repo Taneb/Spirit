bb6a8e9eacd5446bd979da5cafe3bb4fb69ccab0
Michael Walker <mike@barrucadu.co.uk>
2013-09-01 13:12:04 +0100

    Pretty-printing of Haskell expressions

--- Spirit/Types.hs
+++ Spirit/Types.hs
@@ -1,4 +1,9 @@
-module Spirit.Types where
+module Spirit.Types (TypeSignature(..) 
+                    , HaskellExpr(..) 
+                    , Name 
+                    , Assumption 
+                    , Goal 
+                    , Pattern) where
 
 -- ADT for type signatures
 data TypeSignature = TVar Int
@@ -13,7 +18,7 @@ infixr 5 :->
 data HaskellExpr = Literal String
                  | Lambda Pattern HaskellExpr
                  | App HaskellExpr HaskellExpr
-                   deriving (Eq, Show)
+                   deriving (Eq)
 
 -- Type of names
 type Name = String
@@ -26,3 +31,23 @@ type Goal = TypeSignature
 
 -- Type of patterns
 type Pattern = String
+
+-- Printing of expressions
+instance Show HaskellExpr where
+    show (Literal s) = s
+
+    show (Lambda p e) = "\\" ++ p ++ " -> " ++ show e
+
+    show (App (Literal f) (Literal e)) = f ++ " " ++ e
+
+    show (App f (Literal e)) | simple f  = show f ++ " " ++ e
+                             | otherwise =  "(" ++ show f ++ ") " ++ e
+
+    show (App f expr) | simple f = show f ++ " (" ++ show expr ++ ")"
+                      | otherwise = "(" ++ show f ++ ") (" ++ show expr ++ ")"
+
+-- Check if a function application is "simple" (not needing parens)
+simple :: HaskellExpr -> Bool
+simple (App f _) = True
+simple (Literal _) = True
+simple _ = False
